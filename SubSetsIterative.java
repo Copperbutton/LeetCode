bb1693b631d960327ede722c0b0dd8d057b573aa
Copperbutton <buptzxk2012@gmail.com>
2014-08-17 18:23:43 -0700

    Formated code style: converting tab to space

--- SubSetsIterative.java
+++ SubSetsIterative.java
@@ -8,18 +8,18 @@
  * [ [3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], [] ]
  **/
 public class SubSetsIterative {
-	public List<List<Integer>> subsets(int[] S) {
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
-		result.add(new ArrayList<Integer>());
-		Arrays.sort(S);
-		for (int next : S) {
-			int resultSize = result.size();
-			for (int i = 0; i < resultSize; i++) {
-				List<Integer> newList = new ArrayList<Integer>(result.get(i));
-				newList.add(next);
-				result.add(newList);
-			}
-		}
-		return result;
-	}
+    public List<List<Integer>> subsets(int[] S) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<Integer>());
+        Arrays.sort(S);
+        for (int next : S) {
+            int resultSize = result.size();
+            for (int i = 0; i < resultSize; i++) {
+                List<Integer> newList = new ArrayList<Integer>(result.get(i));
+                newList.add(next);
+                result.add(newList);
+            }
+        }
+        return result;
+    }
 }
--- SubSetsWithDupRecrusive.java
+++ SubSetsWithDupRecrusive.java
@@ -10,41 +10,41 @@
  **/
 
 public class SubSetsWithDupRecrusive {
-	public List<List<Integer>> subsetsWithDup(int[] num) {
-		Map<Integer, Integer> appearCount = new HashMap<Integer, Integer>();
-		for (int n : num) {
-			int count = 1;
-			if (appearCount.containsKey(n))
-				count = appearCount.get(n) + 1;
-			appearCount.put(n, count);
-		}
-		Set<Integer> keySet = appearCount.keySet();
-		Integer[] distinctNums = keySet.toArray(new Integer[keySet.size()]);
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
-		Arrays.sort(distinctNums); // For online juege purpose only
-		findSubsetsWithDup(distinctNums, 0, new ArrayList<Integer>(), result,
+    public List<List<Integer>> subsetsWithDup(int[] num) {
+        Map<Integer, Integer> appearCount = new HashMap<Integer, Integer>();
+        for (int n : num) {
+            int count = 1;
+            if (appearCount.containsKey(n))
+                count = appearCount.get(n) + 1;
+            appearCount.put(n, count);
+        }
+        Set<Integer> keySet = appearCount.keySet();
+        Integer[] distinctNums = keySet.toArray(new Integer[keySet.size()]);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(distinctNums); // For online juege purpose only
+        findSubsetsWithDup(distinctNums, 0, new ArrayList<Integer>(), result,
                            appearCount);
-		return result;
-	}
+        return result;
+    }
     
-	private void findSubsetsWithDup(Integer[] distinctNums, int depth,
+    private void findSubsetsWithDup(Integer[] distinctNums, int depth,
                                     List<Integer> path, List<List<Integer>> result,
                                     Map<Integer, Integer> appearCount) {
-		if (depth == distinctNums.length) {
-			result.add(new ArrayList<Integer>(path));
-			return;
-		}
+        if (depth == distinctNums.length) {
+            result.add(new ArrayList<Integer>(path));
+            return;
+        }
         
-		int nextNum = distinctNums[depth];
-		int appear = appearCount.get(nextNum);
-		for (int i = 0; i <= appear; i++) {
-			findSubsetsWithDup(distinctNums, depth + 1, path, result,
+        int nextNum = distinctNums[depth];
+        int appear = appearCount.get(nextNum);
+        for (int i = 0; i <= appear; i++) {
+            findSubsetsWithDup(distinctNums, depth + 1, path, result,
                                appearCount);
-			path.add(nextNum);
-		}
+            path.add(nextNum);
+        }
         
-		for (int i = 0; i <= appear; i++) {
-			path.remove(path.size() - 1);
-		}
-	}
+        for (int i = 0; i <= appear; i++) {
+            path.remove(path.size() - 1);
+        }
+    }
 }
