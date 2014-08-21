f074de763f9878e22c4eb5b04b8cbf97e1ec5b4e
Copperbutton <buptzxk2012@gmail.com>
2014-08-21 11:48:30 -0700

    Solved Best Time To Buy and Sell Stock problem

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,22 @@
+/**
+ * Say you have an array for which the ith element is the price of a given stock
+ * on day i.
+ * 
+ * If you were only permitted to complete at most one transaction (ie, buy one
+ * and sell one share of the stock), design an algorithm to find the maximum
+ * profit.
+ */
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1)
+            return 0;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(prices[i] - minPrice, maxProfit);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+}
