bdd96715831dd2cafc1eb5998b5aa5f9b8c1a0e8
Copperbutton <buptzxk2012@gmail.com>
2014-08-26 11:48:07 -0700

    Solved Remove Element Problem

---
+++ RemoveElement.java
@@ -0,0 +1,20 @@
+/**
+ * Given an array and a value, remove all instances of that value in place and
+ * return the new length.
+ * 
+ * The order of elements can be changed. It doesn't matter what you leave beyond
+ * the new length.
+ */
+
+public class RemoveElement {
+    public int removeElement(int[] A, int elem) {
+        int start = 0, end = 0;
+        while (end < A.length) {
+            if (A[end] != elem)
+                A[start++] = A[end++];
+            else
+                end++;
+        }
+        return start;
+    }
+}
