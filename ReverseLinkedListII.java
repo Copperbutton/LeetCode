6bab49f8c2373083311fc024d457b3070ef8a31b
Copperbutton <buptzxk2012@gmail.com>
2014-08-27 22:26:33 -0700

    Solved ReverseLinkedListII problem

---
+++ ReverseLinkedListII.java
@@ -0,0 +1,39 @@
+/**
+ * Reverse a linked list from position m to n. Do it in-place and in one-pass.
+ * 
+ * For example: Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+ * 
+ * return 1->4->3->2->5->NULL.
+ * 
+ * Note: Given m, n satisfy the following condition: 1 ≤ m ≤ n ≤ length of list.
+ * 
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class ReverseLinkedListII {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 0; i < m - 1; i++)
+            prev = prev.next;
+
+        ListNode curr = prev.next;
+        ListNode next = curr.next;
+        for (int count = m; count < n; count++) {
+            curr.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+            next = curr.next;
+        }
+        return dummy.next;
+    }
+}
