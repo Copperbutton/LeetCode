ef8de19f240a0243542a11dc59f53a298032b456
Copperbutton <buptzxk2012@gmail.com>
2014-09-02 12:37:45 -0700

    Solved Flattern Binary Tree To Linked List problem

---
+++ FlatternBinaryTreeToLinkedList.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class FlatternBinaryTreeToLinkedList {
+    public void flatten(TreeNode root) {
+        TreeNode curr = root, tmp = null;
+        while (curr != null) {
+            if (curr.left != null){
+                tmp = curr.left;
+                while (tmp.right != null)
+                    tmp = tmp.right;
+                tmp.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+    }
+}
