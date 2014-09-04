2e1dcf6944ebdc1c20c90bb85802f6edb720712d
Copperbutton <buptzxk2012@gmail.com>
2014-09-03 18:11:02 -0700

    Solved Merge Two sorted Lists problem

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -18,25 +18,20 @@ public class MergeTwoSortedLists {
         ListNode dummy = new ListNode(-1);
         dummy.next = l1;
         ListNode prev = dummy, p1 = l1, p2 = l2;
-        while (p1 != null || p2 != null) {
-            if (p1 != null && p2 != null) {
-                if (p1.val > p2.val) {
-                    prev.next = p2;
-                    p2 = p2.next;
-                    prev.next.next = p1;
-                    prev = prev.next;
-                } else {
-                    prev = prev.next;
-                    p1 = p1.next;
-                }
-            } else if (p2 != null) {
+        while (p1 != null && p2 != null) {
+            if (p1.val > p2.val) {
                 prev.next = p2;
                 p2 = p2.next;
                 prev.next.next = p1;
                 prev = prev.next;
-            } else
-                break;
+            } else {
+                prev = prev.next;
+                p1 = p1.next;
+            }
         }
+        
+        if (p2 != null)
+            prev.next = p2;
         return dummy.next;
     }
 }
