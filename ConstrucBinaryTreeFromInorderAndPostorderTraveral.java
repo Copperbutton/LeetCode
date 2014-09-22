dbf6629936ceacd9570fb9f3eee24a2a3f83243c
Copperbutton <buptzxk2012@gmail.com>
2014-09-21 20:20:40 -0700

    Refined Constructed from inorder and post order code

--- ConstrucBinaryTreeFromInorderAndPostorderTraveral.java
+++ ConstrucBinaryTreeFromInorderAndPostorderTraveral.java
@@ -1,12 +1,4 @@
 /**
- * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
- * 
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; next = null; }
- * }
  * Definition for binary tree
  * public class TreeNode {
  *     int val;
@@ -15,31 +7,17 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-public class ConstrucBinaryTreeFromInorderAndPostorderTraveral {
-    public TreeNode sortedListToBST(ListNode head) {
-        if (head == null)
+public class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int len = inorder.length;
+        if (len == 0)
             return null;
-
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode prev = dummy, slow = head, fast = head.next == null ? null
-                : head.next;
-        while (fast != null && fast.next != null) {
-            prev = prev.next;
-            slow = slow.next;
-            fast = fast.next;
-            fast = fast.next;
-        }
-
-        TreeNode root = new TreeNode(slow.val);
-        prev.next = null;
-        root.left = sortedListToBST(dummy.next);
-
-        prev = slow;
-        slow = slow.next;
-        prev.next = null;
-        root.right = sortedListToBST(slow);
+        TreeNode root = new TreeNode(postorder[len - 1]);
+        int index = 0;
+        while (index < len && inorder[index] != postorder[len - 1])
+            index ++;
+        root.left = buildTree(Arrays.copyOfRange(inorder, 0, index), Arrays.copyOfRange(postorder, 0, index));
+        root.right = buildTree(Arrays.copyOfRange(inorder, index + 1, len), Arrays.copyOfRange(postorder, index, len - 1));
         return root;
     }
 }
