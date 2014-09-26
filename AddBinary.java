616f5fc9a1de510371b6197060b748a34b59e9c3
Copperbutton <buptzxk2012@gmail.com>
2014-09-26 13:55:42 -0700

    Refined Code

--- AddBinary.java
+++ AddBinary.java
@@ -5,24 +5,27 @@
  */
 
 public class AddBinary {
-    public String addBinary(String a, String b) {
-        char[] aChs = a.toCharArray(), bChs = b.toCharArray();
-        int resultLen = Math.max(a.length(), b.length()) + 1;
-        char[] result = new char[resultLen];
-        Arrays.fill(result, '0');
-
+public String addBinary(String a, String b) {
+        if (a == null || b == null)
+            return a == null ? b : a;
+        int aLen = a.length();
+        int bLen = b.length();
+        char[] ret = new char[Math.max(aLen, bLen) + 1];
+        int indexA = aLen - 1;
+        int indexB = bLen - 1;
+        int indexS = ret.length - 1;
         int carry = 0;
-        int aIndx = aChs.length - 1, bIndx = bChs.length - 1, rIndx = resultLen - 1;
-        while (aIndx >= 0 || bIndx >= 0 || rIndx >= 0) {
+        while (indexA >= 0 || indexB >= 0 || indexS >= 0) {
             int sum = carry;
-            if (aIndx >= 0)
-                sum += aChs[aIndx--] - '0';
-            if (bIndx >= 0)
-                sum += bChs[bIndx--] - '0';
-            result[rIndx--] += sum % 2;
-            carry = sum / 2;
+            if (indexA >= 0)
+                sum += (a.charAt(indexA--) - '0');
+            if (indexB >= 0)
+                sum += (b.charAt(indexB--) - '0');
+            ret[indexS--] = (char)('0' + sum % 2);
+            carry = sum/2;
         }
-        return new String(result[0] == '0' ? Arrays.copyOfRange(result, 1,
-                resultLen) : result);
+        if (ret[0] == '0')
+            ret = Arrays.copyOfRange(ret, 1, ret.length);
+        return new String(ret);
     }
 }
