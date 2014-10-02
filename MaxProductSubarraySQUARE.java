dc9d4d5c34378e44a4730d1b3a590418078209b9
Copperbutton <buptzxk2012@gmail.com>
2014-10-02 11:09:58 -0700

    Use O(n^2) time, TLE

---
+++ MaxProductSubarraySQUARE.java
@@ -0,0 +1,23 @@
+/**
+ * Find the contiguous subarray within an array (containing at least one number) which has      
+ * the largest product.
+ *
+ * For example, given the array [2,3,-2,4],
+ * the contiguous subarray [2,3] has the largest product = 6.
+ */
+
+public class Solution {
+    public int maxProduct(int[] A) {
+        int size = A.length;
+        int[][] product = new int[size][size];
+        int maxProduct = Integer.MIN_VALUE;
+        for (int row = 0; row < size; row++) {
+            product[row][row] = A[row];
+            for (int col = row + 1; col < size; col ++) {
+                product[row][col] = A[col] * product[row][col - 1];
+                maxProduct = Math.max(product[row][col], maxProduct);
+            }
+        }
+        return maxProduct;
+    }
+}
