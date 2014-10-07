d6f22f9c495b703d9b871ab63b2eb99c4a595760
Copperbutton <buptzxk2012@gmail.com>
2014-10-07 11:47:02 -0700

    Refined Code

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -5,18 +5,24 @@
 
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        String cmpBase = strs.length > 0 ? strs[0] : "";
-        int endIndex = cmpBase.length() - 1;
+        if (strs == null || strs.length == 0)
+            return "";
+            
+        String base = strs[0];
+        int lenLimit = base.length();
         for (int i = 1; i < strs.length; i++) {
-            String cmpNext = strs[i];
-            endIndex = Math.min(endIndex, cmpNext.length() - 1);
-            for (int j = 0; j <= endIndex; j++) {
-                if (cmpBase.charAt(j) != cmpNext.charAt(j)) {
-                    endIndex = j - 1;
-                    break;
-                }
-            }
+            lenLimit = findCommonPrefixEnd(base, strs[i], lenLimit);
+            if (lenLimit == 0)
+                break;
         }
-        return cmpBase.substring(0, endIndex + 1);
+        return base.substring(0, lenLimit);
+    }
+    
+    private int findCommonPrefixEnd(String first, String second, int lenLimit) {
+        int len = Math.min(Math.min(first.length(), second.length()), lenLimit);
+        int index = 0;
+        while (index < len && first.charAt(index) == second.charAt(index))
+            index++;
+        return index;
     }
 }
