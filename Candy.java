823e021f0df19ff5aba4e8251b7c2a6668675f2c
Copperbutton <buptzxk2012@gmail.com>
2014-10-08 15:43:12 -0700

    Refined Code

--- Candy.java
+++ Candy.java
@@ -11,26 +11,23 @@
 
 public class Candy {
     public int candy(int[] ratings) {
-        int len = ratings.length;
-        if (len == 0)
+        if (ratings == null || ratings.length == 0)
             return 0;
-
+            
+        int len = ratings.length;
         int[] candies = new int[len];
-        Arrays.fill(candies, 1);
-
+        candies[0] = 1;
         for (int i = 1; i < len; i++) {
-            if (ratings[i] > ratings[i - 1])
-                candies[i] = candies[i - 1] + 1;
+            candies[i] = ratings[i] > ratings[i - 1] ? candies[i - 1] + 1 : 1;
         }
-
+        
         int sum = candies[len - 1];
-        for (int i = len - 2, inc = 1; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1])
-                candies[i] = Math.max(++inc, candies[i]);
-            else
-                inc = 1;
+        for (int i = len - 2; i>= 0; i--) {
+            int candy = ratings[i] > ratings[i + 1] ? candies[i + 1] + 1 : 1;
+            candies[i] = Math.max(candy, candies[i]);
             sum += candies[i];
         }
+        
         return sum;
     }
 }
