b5655fb152eb805302cdb2258a16463e9e09b565
Copperbutton <buptzxk2012@gmail.com>
2014-10-08 21:57:05 -0700

    Refined Code

--- MaximumDepthOfBinaryTree.java
+++ MaximumDepthOfBinaryTree.java
@@ -10,24 +10,8 @@
 
 public class Solution {
     public int maxDepth(TreeNode root) {
-        return findMaxDepth(root);
-    }
-    
-    private int findMaxDepth(TreeNode root) {
         if (root == null)
             return 0;
-        
-        if (root.left == null && root.right == null)
-            return 1;
-        
-        int left = findMaxDepth(root.left);
-        int right = findMaxDepth(root.right);
-        
-        int max = 0;
-        if (left > 0)
-            max = Math.max(max, left);
-        if (right > 0)
-            max = Math.max(max, right);
-        return max + 1;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
