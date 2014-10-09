47591259c61b9217cbca3d90ad3d5aed7421db7f
Copperbutton <buptzxk2012@gmail.com>
2014-10-08 20:57:47 -0700

    Refined Code

--- SortList.java
+++ SortList.java
@@ -14,41 +14,38 @@
 
 public class SortList {
     public ListNode sortList(ListNode head) {
-        ListNode slow = head, fast = head == null ? null : head.next;
+        if (head == null || head.next == null)
+            return head;
+            
+        ListNode slow = head;
+        ListNode fast = head.next;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next;
             fast = fast.next;
         }
-
-        if (slow != null && slow.next != null) {
-            fast = slow.next;
-            slow.next = null;
-            ListNode p1 = sortList(head);
-            ListNode p2 = sortList(fast);
-            head = mergeSortedList(p1, p2);
-        }
-        return head;
+        
+        fast = slow.next;
+        slow.next = null;
+        head = sortList(head);
+        fast = sortList(fast);
+        return mergeSortedList(head, fast);
     }
-
-    private ListNode mergeSortedList(ListNode first, ListNode second) {
+    
+    public ListNode mergeSortedList(ListNode first, ListNode second) {
         ListNode dummy = new ListNode(-1);
         dummy.next = first;
-        ListNode prev = dummy;
-        while (first != null && second != null) {
-            if (second.val < first.val) {
-                prev.next = second;
-                second = second.next;
-                prev = prev.next;
-                prev.next = first;
-            } else {
-                prev = prev.next;
-                first = first.next;
-            }
+        ListNode secondCurr = second;
+        ListNode prevInsert = dummy;
+        while (secondCurr != null) {
+            while(prevInsert.next != null && prevInsert.next.val < secondCurr.val)
+                prevInsert = prevInsert.next;
+            second = second.next;
+            secondCurr.next = prevInsert.next;
+            prevInsert.next = secondCurr;
+            secondCurr = second;
+            prevInsert = prevInsert.next;
         }
-        if (second != null)
-            prev.next = second;
-
         return dummy.next;
     }
 }
