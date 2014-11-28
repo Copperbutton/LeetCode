87c6349fb5ccc21985a6efdb6c1ff2e63fbd115c
Copperbutton <buptzxk2012@gmail.com>
2014-11-28 01:09:45 -0800

    Reduced time to O(n) by using traversal

--- FlatternBinaryTreeToLinkedList.java
+++ FlatternBinaryTreeToLinkedList.java
@@ -10,17 +10,28 @@
 
 public class FlatternBinaryTreeToLinkedList {
     public void flatten(TreeNode root) {
-        TreeNode curr = root, tmp = null;
-        while (curr != null) {
-            if (curr.left != null){
-                tmp = curr.left;
-                while (tmp.right != null)
-                    tmp = tmp.right;
-                tmp.right = curr.right;
-                curr.right = curr.left;
-                curr.left = null;
-            }
-            curr = curr.right;
+        if (root == null)
+            return;
+        List<TreeNode> preorder = new ArrayList<TreeNode> ();
+        preorder.add(root);
+        flattern(root, preorder);
+    }
+    
+    private void flattern(TreeNode root, List<TreeNode> preorder) {
+        if (root.left == null && root.right == null) {
+            preorder.clear();
+            preorder.add(root);
+            return;
         }
+        
+        if (root.left != null) {
+            flattern(root.left, preorder);
+            preorder.get(0).right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        
+        if (root.right != null)
+            flattern(root.right, preorder);
     }
 }
