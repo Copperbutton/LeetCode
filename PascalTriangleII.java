75f91d4a522ce9d9354446ca48fe3f74113cc4b0
Copperbutton <buptzxk2012@gmail.com>
2014-11-29 12:37:02 -0800

    Changed cache to array so that space complexity decrease

--- PascalTriangleII.java
+++ PascalTriangleII.java
@@ -10,22 +10,20 @@ Could you optimize your algorithm to use only O(k) extra space?
 
 public class PascalTriangleII {
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> result = new ArrayList<Integer>();
-        List<Integer> nextRow = new ArrayList<Integer>();
-
-        result.add(1);
+        List<Integer> result = new ArrayList<Integer> ();
+        if (rowIndex < 0)
+            return result;
+        int[] cache = new int[rowIndex + 2];
+        cache[1] = 1;
         for (int i = 1; i <= rowIndex; i++) {
-            nextRow.add(1);
-            for (int j = 0; j < result.size() - 1; j++) {
-                int nextNum = result.get(j) + result.get(j + 1);
-                nextRow.add(nextNum);
+            int prev = cache[0];
+            for (int j = 1; j <= i + 1; j++) {
+                int tmp = cache[j];
+                cache[j] += prev;
+                prev = tmp;
             }
-            nextRow.add(1);
-            List<Integer> tmp = result;
-            result = nextRow;
-            nextRow = tmp;
-            nextRow.clear();
         }
+        for (int i = 1; i < cache.length; i++)
+            result.add(cache[i]);
         return result;
-    }
 }
