5d709c4d99e39081b21612f24c49027d8a566aab
Copperbutton <buptzxk2012@gmail.com>
2014-12-03 11:54:52 -0800

    Removed unnecessary codes

--- SpiralMatrixII.java
+++ SpiralMatrixII.java
@@ -10,35 +10,30 @@
 
 public class SpiralMatrixII {
     public int[][] generateMatrix(int n) {
-        int[][] result = new int[n][n];
-        int startRow = 0, endRow = n - 1;
-        int startCol = 0, endCol = n - 1;
+        if (n < 0)
+            return null;
+        
+        int[][] matrix = new int[n][n];
+        int startR = 0, endR = n - 1;
+        int startC = 0, endC = n - 1;
         int count = 1;
         while (count <= n * n) {
-            for (int col = startCol; col <= endCol; col++) {
-                result[startRow][col] = count;
-                count++;
-            }
-            startRow++;
-
-            for (int row = startRow; row <= endRow; row++) {
-                result[row][endCol] = count;
-                count++;
-            }
-            endCol--;
-
-            for (int col = endCol; col >= startCol; col--) {
-                result[endRow][col] = count;
-                count++;
-            }
-            endRow--;
-
-            for (int row = endRow; row >= startRow; row--) {
-                result[row][startCol] = count;
-                count++;
-            }
-            startCol++;
+            for (int i = startC; i <= endC; i++)
+                matrix[startR][i] = count ++;
+            ++ startR;
+            
+            for (int i = startR; i <= endR; i++)
+                matrix[i][endC] = count ++;
+            -- endC;
+            
+            for (int i = endC; i >= startC; i--)
+                matrix[endR][i] = count ++;
+            -- endR;
+            
+            for (int i = endR; i>= startR; i--)
+                matrix[i][startC] = count ++;
+            ++ startC;
         }
-        return result;
+        return matrix;
     }
 }
